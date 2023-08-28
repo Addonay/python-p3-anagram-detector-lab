e3aa1893b0f2d37c68d8ec1b1c685c8e369a46eb
Addonay <addonay.osoro@student.moringaschool.com>
2023-08-28 10:31:13 +0300

    solved the lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,20 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, word_list):
+        anagrams = []
+        sorted_word = sorted(self.word)
+
+        for candidate in word_list:
+            candidate_lower = candidate.lower()
+            if candidate_lower != self.word and sorted(candidate_lower) == sorted_word:
+                anagrams.append(candidate)
+
+        return anagrams
+
+
+listen = Anagram("listen")
+result = listen.match(['enlists', 'google', 'inlets', 'banana'])
+print(result) 
